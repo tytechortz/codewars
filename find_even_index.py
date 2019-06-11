95e9d74dfa41eb58540a3f683ea38ecda1d9b356
James Swank <jmswank7@gmail.com>
2019-06-11 13:45:50 -0600

    find_even_index passes tests

---
+++ find_even_index.py
@@ -0,0 +1,15 @@
+def find_even_index(arr):
+    i = 0
+    answer = 0
+    while(i <= len(arr)):
+        if sum(arr[:i+1]) == sum(arr[i:]):
+            answer = int(i)
+            break
+        else:
+            answer = -1
+            i += 1
+   
+    return answer
+   
+
+find_even_index([1,2,3,4,3,2,1])
