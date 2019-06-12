471c9ef2f1ad78d9ac17d0441ebc2c7cd410022c
James Swank <jmswank7@gmail.com>
2019-06-12 12:15:08 -0600

    passes 13 tests

--- dubstep.py
+++ dubstep.py
@@ -1,7 +1,8 @@
-import re
-
 def song_decoder(song):
     song = song.replace("WUB", " ")
+    song = song.lstrip()
+    song = song.rstrip()
     print(song)
+    return song
 
 song_decoder("AWUBWUBWUBBWUBWUBWUBC  ")
