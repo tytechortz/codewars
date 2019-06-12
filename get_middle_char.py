702bd07ffecb4b67792657477497c5c7551d6568
James Swank <jmswank7@gmail.com>
2019-06-12 13:02:21 -0600

    passes tests

--- get_middle_char.py
+++ get_middle_char.py
@@ -3,9 +3,10 @@ def get_middle(s):
     if length % 2 == 0:
         answer = s[int(length/2)- 1] + s[int(length/2)] 
         print(answer)
+    else:
+        answer = s[int(length/2)]
+        print(answer)
 
 
 
-
-
-get_middle("test")
+get_middle("testy")
