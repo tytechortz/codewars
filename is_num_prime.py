3114924330534bfa7e4f8e9bfd585ddd87bb6d31
James Swank <jmswank7@gmail.com>
2019-06-13 15:23:10 -0600

    passes tests

--- is_num_prime.py
+++ is_num_prime.py
@@ -1,8 +1,23 @@
 def is_prime(num):
     divs = []
-    for x in range(3,int(num**.5)+1):
-        if x**2 <  num:
-            divs.append(x)  
-    print(divs)
+    answ = 0
+    falses = [0,1,4]
+    if num in falses or num < 0:
+        return False
+    else:
+        for x in range(2,int(num**.5)+1):
+            if x**2 <=  num:
+                divs.append(x)  
+        print(divs)
 
-is_prime(31)
+        for y in divs:
+            if num % y == 0:
+                answ += 1
+    
+        if answ == 0:
+            return True
+        else:
+            return False
+        
+
+is_prime(73)
