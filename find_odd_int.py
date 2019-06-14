debfe6a8fa3376ced2a62084a29d940da8d3c328
James Swank <jmswank7@gmail.com>
2019-06-14 15:13:04 -0600

    passes tests

--- find_odd_int.py
+++ find_odd_int.py
@@ -1,18 +1,6 @@
 def find_it(seq):
-    seq.sort()
-    print(seq)
-    counter = 0
-    for x in range(len(seq)):
-        # print(seq[x])
-        if seq[x] == seq[x + 1]:
-            counter += 1
-            if counter % 2 == 0:
-                # print(counter)
-                print(seq[x])
-                return seq[x]
-                # break
-        else:
-            counter = 0
-
-
-find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+    for x in seq:
+        if seq.count(x) % 2 != 0:
+            print(x)
+            return x
+find_it([-2, -2,-1,-1,1])
