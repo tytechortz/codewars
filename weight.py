387a0492b1cf1152cb17b74a4670ce0c64f6a16a
James Swank <jmswank7@gmail.com>
2019-06-17 08:34:21 -0600

    list of first digits of each number

--- weight.py
+++ weight.py
@@ -1,40 +1,38 @@
-import operator
-
 def order_weight(string):
     mystring = ''
     answer = []
     arranged_list = []
+    first_digit = []
     string = string.split()
-    string = [int(i) for i in string]
-    # print(len(string[0]))
-    for n in string:
+    int_list = [int(i) for i in string]
+
+    for numbers in string:
+        first_digit.append(int(numbers[0]))
+    print(first_digit)
+
+  
+    
+    for n in int_list:
         s = 0
         v = n
-        i = string.index(n)
+        # first_digit = n[0] 
         while n:
             dig = n % 10
             s += dig
             n = n//10
-        # print(s)
-        arranged_list.append((s,v,i))
-       
+    
+        arranged_list.append((s,v))
         print(arranged_list)
-    def sortSecond(val):
-        return val[0]
 
-    arranged_list.sort(key = sortSecond)
-    # print(arranged_list)
-    # print(arranged_list[0])
+    # for numbers in string:
+    #     arranged_list.append()
+    
+    arranged_list = sorted(arranged_list, key=lambda x: (x[0], -x[1]))
+
     for l in range(len(arranged_list)):
-        print(arranged_list[l][1])
         answer.append(arranged_list[l][1])
     
-    #     answer = " ".join(arranged_list[l][0])
-    # print(answer)
     mystring = " ".join(map(str,answer))
     print(mystring)
    
-
-    
-
 order_weight("2000 10003 1234000 44444444 9999 11 11 22 123")
