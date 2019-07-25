690b8254821555f5cede1a8949419a0550775851
James Swank <jmswank7@gmail.com>
2019-07-25 10:43:26 -0600

    creates customer

--- class_practice/bank_account.py
+++ class_practice/bank_account.py
@@ -1,14 +1,21 @@
-# class Customer():
-#     def __init__(self, balance=0):
-#         self.name = name 
-#         self.balance = balance
+class Customer():
+    def __init__(self, name, balance=0):
+        self.name = name 
+        self.balance = balance
 
 
-#     def withdrawl(self, ):
-#         self.balance -
+    def withdrawl(self, x):
+        self.balance - x
 
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+c1 = Customer("Bill", 1000)
 
+print(c1.balance)
+
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# p1 = Person("John", 36)
+# print(p1.name)
+# print(p1.age)
