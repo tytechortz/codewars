d1cead95d9b9a40f3a86e1578160e58d6735a41b
James Swank <jmswank7@gmail.com>
2019-07-25 10:23:13 -0600

    starts person class

---
+++ class_practice/bank_account.py
@@ -0,0 +1,14 @@
+# class Customer():
+#     def __init__(self, balance=0):
+#         self.name = name 
+#         self.balance = balance
+
+
+#     def withdrawl(self, ):
+#         self.balance -
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
