8517c38e174fac0a8a81a0939f51a6d83ca0e355
James Swank <jmswank7@gmail.com>
2019-08-26 14:39:50 -0600

    embed

--- python_practice.py
+++ python_practice.py
@@ -1,13 +1,17 @@
-input_list = [5,6,2,1,6,7,10,12]
+# input_list = [5,6,2,1,6,7,10,12]
 
-def div_by_five(num):
-    if num % 5 == 0:
-        return True
-    else:
-        return False
+# def div_by_five(num):
+#     if num % 5 == 0:
+#         return True
+#     else:
+#         return False
 
-# xyz = (i for i in input_list if div_by_five(i))
-# print(list(xyz))
+# # xyz = (i for i in input_list if div_by_five(i))
+# # print(list(xyz))
 
-xyz = [i for i in input_list if div_by_five(i)]
-print(xyz)
+# xyz = [i for i in input_list if div_by_five(i)]
+# print(xyz)
+
+[print(i) for i in range(5)]
+
+[[print(i,ii) for ii in range(3)] for i in range(5)]
